ab478af7291dadcd2c557256ae0409fc7fb81dd1
Syadon <bokagabor4@gmail.com>
2022-09-17 19:20:37 +0200

    api tests done for all, filter, id, list endpoints

--- src/__test__/movies.test.ts
+++ src/__test__/movies.test.ts
@@ -1,9 +1,10 @@
 import supertest from 'supertest'
+import {Express} from 'express'; 
 import createServer from "../utils/server"
 import {connect} from "../db/database"
 import mongoose from 'mongoose';
 
-const app = createServer();
+const app : Express = createServer();
 
 describe('movies', () => {
 beforeAll(async () => {
@@ -14,6 +15,82 @@ beforeAll(async () => {
         describe('given the database is not empty', () => {
             it('should return a 200 OK status', async () => {
                 await supertest(app).get("/movie/all").expect(200);
+            });
+        })
+    })
+
+    describe('get all movies', () => {
+        describe('given the database is empty', () => {
+            it('should return a 404 not found status', async () => {
+                await supertest(app).get("/movie/all").expect(404);
+            });
+        })
+    })
+
+    describe('get all movies for list', () => {
+        describe('given the database is not empty', () => {
+            it('should return a 200 OK status', async () => {
+                await supertest(app).get("/movie/list").expect(200);
+            });
+        })
+    })
+
+    describe('get all movies for list', () => {
+        describe('given the database is empty', () => {
+            it('should return a 404 not found status', async () => {
+                await supertest(app).get("/movie/list").expect(404);
+            });
+        })
+    })
+
+    describe('get a movie by id', () => {
+        describe('given the movie with specified id exists', () => {
+            it('should return a 200 OK status', async () => {
+                const id : string = "6314db85afca4782eb65aa78";
+                const {body, statusCode} = await supertest(app)
+                .get(`/movie/${id}`);
+
+                expect(statusCode).toBe(200);
+                expect(body[0]._id).toBe(id);
+            });
+        })
+    })
+
+    describe('get a movie by id', () => {
+        describe('given the movie with specified id does not exists', () => {
+            it('should return a 404 not found status', async () => {
+                const id : string = "6314db85afca4782eb659999";
+                const {body, statusCode} = await supertest(app)
+                .get(`/movie/${id}`);
+
+                expect(statusCode).toBe(404);
+                expect(body).toEqual({});
+            });
+        })
+    })
+
+    describe('get specific movies with filters', () => {
+        describe('given there are movies that the filter matches', () => {
+            it('should return a 200 OK status', async () => {
+                const filter : string = "Csillagok háborúja";
+                const {body, statusCode} = await supertest(app)
+                .get(`/movie/filter/${filter}`);
+
+                expect(statusCode).toBe(200);
+                expect(body).not.toEqual({});
+            });
+        })
+    })
+
+    describe('get specific movies with filters', () => {
+        describe('given there are no movies existing that the filter matches', () => {
+            it('should return a 404 not found status', async () => {
+                const filter : string = "sosemleszilyenmovie";
+                const {body, statusCode} = await supertest(app)
+                .get(`/movie/filter/${filter}`);
+
+                expect(statusCode).toBe(404);
+                expect(body).toEqual({});
             });
         })
     })
