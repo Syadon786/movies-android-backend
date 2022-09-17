993edc1877d2307cee690fa0675355e08c6e9e58
Syadon <bokagabor4@gmail.com>
2022-09-17 16:58:21 +0200

    connect to database

--- src/__test__/movies.test.ts
+++ src/__test__/movies.test.ts
@@ -1,13 +1,18 @@
 import supertest from 'supertest'
 import createServer from "../utils/server"
+import {connect} from "../db/database"
 
 const app = createServer();
 
-describe('movies/all', () => {
+describe('movies', () => {
+beforeAll(async () => {
+    await connect();
+})
+    
     describe('get all movies', () => {
         describe('given the database is not empty', () => {
             it('should return a 200 OK status', async () => {
-                await supertest(app).get("/movie/all").expect(200)
+                await supertest(app).get("/movie/all").expect(200);
             });
         })
     })
